2b33b97182cb6c3b0580289c874e9a5c019f1b7f
birkin <birkin.diana@gmail.com>
2017-08-28 12:40:12 -0400

    initial commit.

---
+++ hay_aeon_exclusions.js
@@ -0,0 +1,10 @@
+/*
+ * Global variable used by javascript embedded in classic-josiah, and in new-josiah, to prevent a hay-aeon link from appearing.
+ */
+
+var hay_aeon_exclusions = [
+    "HAY ANNEX TEMP",
+    "HAY ARCHIVES MANUSCRIPTS",
+    "HAY COURSE RESERVES",
+    "HAY MANUSCRIPTS"
+    ];
